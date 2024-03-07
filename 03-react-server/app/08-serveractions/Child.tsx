bbc0b7b4fc35f946aba966be0c9bde31dba7dd9c
Jonas Bandi <dev@jonasbandi.net>
2024-03-07 16:09:48 +0100

    Cleanup

--- 03-react-server/app/08-serveractions/Child.tsx
+++ 03-react-server/app/08-serveractions/Child.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { updateCountServerWithValue } from "@/app/02-backend/serverData";
-
+import styles from "./Child.module.css";
 // type ChildProps = { updateFn: () => Promise<void> };
 
 export default function Child() {
@@ -11,14 +11,7 @@ export default function Child() {
 
   console.log("rendering Child");
   return (
-    <div
-      style={{
-        border: "3px solid black",
-        padding: 30,
-        marginTop: 30,
-        backgroundColor: "cyan",
-      }}
-    >
+    <div className={styles.wrapper}>
       <h2 style={{ margin: 0 }}>Display of Child.</h2>
       <p style={{ marginTop: 0 }}>Rendering on client</p>
       <button onClick={handleClick}>Update</button>

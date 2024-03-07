ef039a2e58de6bf3f42b55b736279b93f280a4c5
Jonas Bandi <dev@jonasbandi.net>
2024-03-07 15:19:08 +0100

    Preparation

--- 03-react-server/app/05-counter/Counter.tsx
+++ 03-react-server/app/05-counter/Counter.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
 export default function Counter() {
+  // const [count, setCount] = useState(0);
+  //
+  // function handleClick() {
+  //   setCount((count) => count + 1);
+  // }
+
   return (
     <>
       <h1>?</h1>
